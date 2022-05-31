00e1a62dbc1fea451fb6d3c1528abc4851aaff5f
MatheusDario <dario.matheus97@gmail.com>
2022-05-31 19:36:24 -0300

    About LoopsI

---
+++ src/application/LoopsI.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LoopsI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i=1; i<=10; i++) {
+            System.out.println(n + " X " + i + " = " + n * i) ;
+        }
+        bufferedReader.close();
+    }
+}
