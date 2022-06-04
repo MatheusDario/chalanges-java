15125ab1f59aab84b776f639090ff36f91e729ad
MatheusDario <dario.matheus97@gmail.com>
2022-06-04 12:56:15 -0300

    end of file

---
+++ src/application/EndOfFile.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.Scanner;
+
+public class EndOfFile {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int i=1;
+
+        while (sc.hasNext())  {
+            String str = sc.nextLine();
+            System.out.println(i +  " " + str);
+            i++;
+        }
+        sc.close();
+    }
+}
+
